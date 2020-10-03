5dcc65cefd08bbccd1325a70e2a30b6431a0d62c
ffebvay <f.febvay@synakene.fr>
2020-10-03 13:17:30 +0200

    169 Disabling Empty Health Bars

---
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RPG.Attributes
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Health healthComponent = null;
+        [SerializeField] private RectTransform foreground = null;
+
+        void Update()
+        {
+            foreground.localScale = new Vector3(healthComponent.GetFraction(), 1f, 1f);
+        }
+    }
+}
